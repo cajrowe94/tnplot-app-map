e7f2b5e9e1ffcd7b0d91dfab7817a8076b538629
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2022-11-10 01:11:26 -0500

    county data and mpbox work

--- src/js/component/Map.js
+++ src/js/component/Map.js
@@ -2,15 +2,12 @@ import mapboxgl from 'mapbox-gl';
 import config from '../../config.js';
 import { useState, useEffect } from 'react';
 import '../../scss/component/Map.scss';
+import tnCountiesData from '../../data/TN_counties.js';
 
 mapboxgl.accessToken = config.MAPBOX_ACCESS_TOKEN;
 
 export default function Map() {
 	const [map, setMap] = useState();
-
-	/**
-	 * Initialize map
-	 */
 
 	useEffect(() => {
 		setMap(new mapboxgl.Map({
@@ -21,14 +18,42 @@ export default function Map() {
 		}));
 	}, []);
 
-
 	useEffect(() => {
-		if (map) {
-			map.on('load', () => {
-				console.log(map);
+		if (!map) return;
+
+		map.on('load', () => {
+			map.addSource('counties-data', {
+				type: 'geojson',
+				data: tnCountiesData,
+				generateId: true
 			});
-		}
+
+			map.addLayer({
+				id: 'tn-counties',
+				source: 'counties-data',
+				type: 'fill',
+				paint: {
+					'fill-opacity': 0.3,
+					'fill-color': '#7c7d7e',
+					'fill-antialias': true,
+					'fill-outline-color': '#000'
+
+				}
+			});
+		});
+
+		map.on('render', queryCounties);
 	}, [map]);
+
+	const queryCounties = () => {
+		if (!map.getLayer('tn-counties') || !map.loaded()) return;
+
+		let counties = map.queryRenderedFeatures({
+			layers: ['tn-counties']
+		});
+
+		map.off('render', queryCounties);
+	}
 
 	return (
 		<div id="map-container"></div>
